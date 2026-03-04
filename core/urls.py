9402f8faff6ba2b79d79156e29ab12594bdf78a4
Vukay67 <vukay67@gmail.com>
2026-03-04 20:03:13 +0600

    последний проэкт для Itrun

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+﻿from django.urls import path
+from .views import main_page, about_page, contacts_page, login_page, register_page
+
+urlpatterns = [
+    path('', main_page, name="main_page"),
+    path('about/', about_page, name="about_page"),
+    path('contacts', contacts_page, name="contacts_page"),
+    path('login/', login_page, name="login_page"),
+    path('register/', register_page, name="register_page")
+]
---
+++ core/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import AuthenticationForm, RegistrationForm
+
+def main_page(request):
+    context = {
+    }
+    return render(request, "index.html", context)
+
+def about_page(request):
+    return render(request, "about.html", {})
+
+def contacts_page(request):
+    return render(request, "contacts.html", {})
+
+def login_page(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            login(request, form.user)
+            return redirect("main_page")
+    else:
+        form = AuthenticationForm()
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "login.html", context)
+
+def register_page(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+
+            user = User.objects.create_user(
+                email=email,
+                username=username,
+                password=password
+            )
+            login(request, user)
+            return redirect("main_page")
+    else:
+        form = RegistrationForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'register.html', context)
