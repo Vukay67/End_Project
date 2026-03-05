309704dc9adfd2cce8d6c01f8922f53cf671e693
Vukay <ret.gmail.com>
2026-03-06 01:53:27 +0600

    rr

--- core/models.py
+++ core/models.py
@@ -4,3 +4,7 @@ class Anime(models.Model):
     name = models.CharField()
     image = models.ImageField(upload_to="anime_poster")
     description = models.TextField()
+
+class SesonAnime(models.Model):
+    pass
+
